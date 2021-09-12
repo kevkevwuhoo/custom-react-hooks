5380cd82c000d6839b1924f59610ca8c529eb1c5
kwu3 <kwu3@villanova.edu>
2021-09-11 23:15:27 -0500

    Create useDebounce custom hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Toggle from './components/Toggle';
 import Timeout from './components/Timeout';
+import Debounce from './components/Debounce';
 
 function App() {
   // return <Toggle />;
   return <Timeout />;
+  // return <Debounce />;
 }
 
 export default App;
