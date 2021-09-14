c9536ec2d42d6a1eb53ef24d4c42ec1551f9f60f
kwu3 <kwu3@villanova.edu>
2021-09-13 23:07:32 -0500

    Create useUpdateEffect custom hook

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Toggle from './components/Toggle';
 import Timeout from './components/Timeout';
 import Debounce from './components/Debounce';
+import UpdateEffect from './components/UpdateEffect';
 
 function App() {
   // return <Toggle />;
   return <Timeout />;
   // return <Debounce />;
+  // return <UpdateEffect />;
 }
 
 export default App;
